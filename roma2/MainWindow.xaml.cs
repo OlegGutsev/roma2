72f902c576fd560a0fa2851ae6d24a85ebc22134
Oleg Gutsev <olezhka.fanky@mail.ru>
2017-09-29 18:27:05 +0300

    123

--- roma2/MainWindow.xaml.cs
+++ roma2/MainWindow.xaml.cs
@@ -20,9 +20,11 @@ namespace roma2
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+            text.Foreground = new SolidColorBrush(Colors.Blue);
         }
     }
 }
